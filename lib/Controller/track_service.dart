a51c558e7901f08850b1c44ffb7836c9444b3e12
Mukund Dholariya <112772075+mukkund27@users.noreply.github.com>
2022-11-10 16:17:05 +0530

    Update track_service.dart

--- lib/Controller/track_service.dart
+++ lib/Controller/track_service.dart
@@ -1,28 +1,29 @@
+import 'package:dio/dio.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:http/http.dart' as http;
+
 
 import '../Model/lyrics_model.dart';
 import '../Model/track_model.dart';
 
 class GetProductService {
   static Future<TrackModel> GetData() async {
-    http.Response response = await http.get(
-      Uri.parse(
-          "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7"),
-    );
-    return trackModelFromJson(response.body);
+    Response response;
+    var dio = Dio();
+    response = await dio.get(
+        "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7");
+    return trackModelFromJson(response.data);
   }
 
   static Future<LyricsModel> GetLyrics() async {
+    Response response;
+    var dio = Dio();
     final box = GetStorage();
     var id = box.read("track_id");
     // Enter Your Api key
     var api_key = "4191ff2cd4528434029cf33d63363d5c";
-    http.Response response = await http.get(
-      Uri.parse(
-          "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=$id&apikey=$api_key"),
-    );
+    response = await dio.get(
+        "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=$id&apikey=$api_key");
 
-    return lyricsModelFromJson(response.body);
+    return lyricsModelFromJson(response.data);
   }
 }
